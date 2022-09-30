93daef109aeb6b87cd9d6aca71e92298ff9f77a5
NSaiEPS <sainizameps@gmail.com>
2022-09-30 12:32:32 +0530

    Model & Custom Alert

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Alert_Toast from './Components/Alert_Toast';
 import Buttons from './Components/Buttons';
+import Model_CUstomAlert from './Components/Model_CUstomAlert';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <Buttons/> */}
 
-      <Alert_Toast />
+      {/* <Alert_Toast /> */}
+      <Model_CUstomAlert/>
 
 
     </View>
